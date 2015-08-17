c70328d1e6d075f126f0c730d18fe4c3ae5894ea
nsnpartquote <nsnpartquote@gmail.com>
2015-08-16 21:00:15 -0700

    Update database.php

--- ss-application/config/database.php
+++ ss-application/config/database.php
@@ -58,13 +58,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-/*
+
 $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.11.172.2',
 	'username' => 'adminSh1cHll',
 	'password' => 'l-HP6SEl3fXu',
 	'database' => 'nsnsearch',
@@ -83,7 +83,7 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-*/
+
 /*$openshiftsocket = getenv('OPENSHIFT_MYSQL_DB_SOCKET');
 echo "OpenShift socket is [$openshiftsocket]";
  
